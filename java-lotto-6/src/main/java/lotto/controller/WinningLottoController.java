a00b49d679cb87a3e8eb8d061ae7764a8b7accdf
Kim Daehyeon <daehyeon3351@gmail.com>
2024-07-04 05:49:09 +0900

    feat: WinningLotto Controller 구현 - #1
    - WinningLottoService 만 사용
    - Response 에 대한 Handling 은 Application 에서 수행 예정

---
+++ java-lotto-6/src/main/java/lotto/controller/WinningLottoController.java
@@ -0,0 +1,23 @@
+package lotto.controller;
+
+import lotto.dto.request.CalculateLottoRequest;
+import lotto.dto.request.WinningLottoRequest;
+import lotto.dto.response.CalculateLottoResponse;
+import lotto.dto.response.WinningLottoResponse;
+import lotto.service.WinningLottoService;
+
+public class WinningLottoController {
+    private final WinningLottoService winningLottoService;
+
+    public WinningLottoController(WinningLottoService winningLottoService) {
+        this.winningLottoService = winningLottoService;
+    }
+
+    public WinningLottoResponse createWinningLotto(WinningLottoRequest winningLottoRequest) {
+        return winningLottoService.createWinningLotto(winningLottoRequest);
+    }
+
+    public CalculateLottoResponse CalculateLotto(CalculateLottoRequest calculateLottoRequest) {
+        return winningLottoService.calculateWinningLotto(calculateLottoRequest);
+    }
+}
