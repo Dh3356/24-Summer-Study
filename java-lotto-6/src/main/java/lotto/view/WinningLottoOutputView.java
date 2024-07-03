d8f2c385d7d3da19810876b68c365f9074c68d13
Kim Daehyeon <daehyeon3351@gmail.com>
2024-07-04 05:48:12 +0900

    feat: WinningLotto View 구현 - #1
    - Model 인 Lotto, WinningLotto 에 대해서는 알고 있지 않음
    - DTO 를 통해 Controller 와 소통 예정

---
+++ java-lotto-6/src/main/java/lotto/view/WinningLottoOutputView.java
@@ -0,0 +1,80 @@
+package lotto.view;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import lotto.dto.response.CalculateLottoResponse;
+import lotto.dto.response.LottoRankResponse;
+
+/**
+ * 당첨 로또 출력 뷰
+ */
+public class WinningLottoOutputView {
+
+    /**
+     * 당첨 결과 출력
+     *
+     * @param calculateLottoResponse 로또 수익률 계산 응답
+     */
+    public void printWinningResult(CalculateLottoResponse calculateLottoResponse) {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+
+        printLottoRanks(calculateLottoResponse.getLottoRankResponses());
+
+        System.out.println("총 수익률은 " + calculateLottoResponse.getProfitRate() + "%입니다.");
+    }
+
+    /**
+     * 로또 순위별 당첨 횟수를 정렬하여 반환한다.
+     *
+     * @param lottoRankResponses 정렬 전 로또 순위별 당첨 횟수
+     * @return 정렬된 로또 순위별 당첨 횟수
+     */
+    private List<LottoRankResponse> getSortedLottoRanks(HashMap<LottoRankResponse, Integer> lottoRankResponses) {
+        return lottoRankResponses.keySet().stream()
+                .sorted(Comparator.comparingInt(LottoRankResponse::getMatchCount))
+                .toList();
+    }
+
+    /**
+     * 로또 순위별 당첨 결과를 출력한다.
+     *
+     * @param lottoRankResponses 로또 순위별 당첨 횟수
+     */
+    private void printLottoRanks(HashMap<LottoRankResponse, Integer> lottoRankResponses) {
+
+        List<LottoRankResponse> sortedLottoRanks = getSortedLottoRanks(lottoRankResponses);
+
+        sortedLottoRanks.forEach(lottoRankResponse -> {
+            int count = lottoRankResponses.get(lottoRankResponse);
+            printLottoRank(lottoRankResponse, count);
+        });
+    }
+
+    /**
+     * 로또 순위의 당첨 결과를 출력한다.
+     *
+     * @param lottoRankResponse 로또 순위
+     * @param count             당첨 횟수
+     */
+    private void printLottoRank(LottoRankResponse lottoRankResponse, int count) {
+        StringBuilder message = new StringBuilder();
+
+        message.append(lottoRankResponse.getMatchCount()).append("개 일치");
+
+        if (lottoRankResponse.isBonusMatch()) {
+            message.append(", 보너스 볼 일치");
+        }
+
+        message.append(" (")
+                .append(formatPrize(lottoRankResponse.getPrize())).append("원) - ")
+                .append(count).append("개");
+
+        System.out.println(message);
+    }
+
+    private String formatPrize(int prize) {
+        return String.format("%,d", prize);
+    }
+}
