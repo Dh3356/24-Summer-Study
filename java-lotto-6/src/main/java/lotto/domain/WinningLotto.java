21120841df2ceb254f2997883aeddd4143000ca8
Kim Daehyeon <daehyeon3351@gmail.com>
2024-07-04 05:31:22 +0900

    feat: 당첨 로또 구현 - #1
    - 보너스 번호에 대한 Validation 도 추가로 수행
    - 특정 로또가 보너스 번호를 포함하는지 검사하는 메서드 구현

---
+++ java-lotto-6/src/main/java/lotto/domain/WinningLotto.java
@@ -0,0 +1,69 @@
+package lotto.domain;
+
+
+import static lotto.common.constant.ErrorMessageConstants.INVALID_BONUS_NUMBER_CONFLICT;
+import static lotto.common.constant.ErrorMessageConstants.INVALID_BONUS_NUMBER_RANGE;
+import static lotto.common.constant.LottoConstants.LOTTO_BONUS_MAX;
+import static lotto.common.constant.LottoConstants.LOTTO_BONUS_MIN;
+
+import java.util.List;
+
+/**
+ * 당첨 로또
+ * <p>
+ * 보너스 번호를 가지고 있다. 사용자 로또가 보너스 번호를 가지고 있는지 확인할 수 있다.
+ */
+public class WinningLotto extends Lotto {
+    public final int bonusNumber;
+
+    public WinningLotto(final List<Integer> numbers, final int bonusNumber) {
+        super(numbers);
+        validate(bonusNumber);
+        this.bonusNumber = bonusNumber;
+    }
+
+    /**
+     * 사용자 로또가 보너스 번호를 가지고 있는지 확인한다.
+     *
+     * @param lotto 사용자 로또
+     * @return 보너스 번호를 가지고 있는지 여부
+     */
+    public boolean containsBonusNumber(Lotto lotto) {
+        return lotto.contains(bonusNumber);
+    }
+
+    /**
+     * 생성자로 받은 매개변수들을 검증한다.
+     *
+     * @param bonusNumber 확인할 보너스 번호
+     */
+    private void validate(int bonusNumber) {
+        validateBonusNumber(bonusNumber);
+    }
+
+    /**
+     * 보너스 번호의 유효성을 검증한다.
+     *
+     * @param bonusNumber 확인할 보너스 번호
+     */
+    private void validateBonusNumber(int bonusNumber) {
+        validateBonusNumberRange(bonusNumber);
+        validateNumbersConflict(numbers);
+    }
+
+    private void validateBonusNumberRange(int bonusNumber) {
+        if (bonusNumber < LOTTO_BONUS_MIN || bonusNumber > LOTTO_BONUS_MAX) {
+            throw new IllegalArgumentException(INVALID_BONUS_NUMBER_RANGE);
+        }
+    }
+
+    private void validateNumbersConflict(List<Integer> numbers) {
+        if (numbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException(INVALID_BONUS_NUMBER_CONFLICT);
+        }
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
+}
