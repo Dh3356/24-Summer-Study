a8d84463fe35404ffc73b8680cc53238dce065f9
Kim Daehyeon <daehyeon3351@gmail.com>
2024-07-04 05:21:40 +0900

    feat: 상수 정의 - #1

---
+++ java-lotto-6/src/main/java/lotto/common/constant/LottoConstants.java
@@ -0,0 +1,15 @@
+package lotto.common.constant;
+
+import static lotto.common.constant.NumberConstants.FORTY_FIVE;
+import static lotto.common.constant.NumberConstants.ONE;
+import static lotto.common.constant.NumberConstants.SIX;
+import static lotto.common.constant.NumberConstants.THOUSAND;
+
+public class LottoConstants {
+    public static final int LOTTO_PRICE = THOUSAND;// 로또 한 장의 가격
+    public static final int LOTTO_MIN = ONE;// 로또 번호의 최소값
+    public static final int LOTTO_MAX = FORTY_FIVE;// 로또 번호의 최대값
+    public static final int LOTTO_SIZE = SIX;// 로또 번호의 크기
+    public static final int LOTTO_BONUS_MIN = ONE;// 보너스 번호의 최소값
+    public static final int LOTTO_BONUS_MAX = FORTY_FIVE;// 보너스 번호의 최대값
+}
