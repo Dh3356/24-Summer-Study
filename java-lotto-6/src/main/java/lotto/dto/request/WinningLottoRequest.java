8afa6648ea4e31b94915a19b1592c4f4a88112ad
Kim Daehyeon <daehyeon3351@gmail.com>
2024-07-04 05:42:37 +0900

    feat: 당첨 번호 생성에 사용할 DTO 구현 - #1

---
+++ java-lotto-6/src/main/java/lotto/dto/request/WinningLottoRequest.java
@@ -0,0 +1,27 @@
+package lotto.dto.request;
+
+import java.util.List;
+
+/**
+ * 당첨 번호 요청 데이터
+ * <p>
+ *
+ * @see CalculateLottoRequest
+ */
+public class WinningLottoRequest {
+    private final List<Integer> numbers;
+    private final int bonusNumber;
+
+    public WinningLottoRequest(List<Integer> numbers, int bonusNumber) {
+        this.numbers = numbers;
+        this.bonusNumber = bonusNumber;
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
+}
