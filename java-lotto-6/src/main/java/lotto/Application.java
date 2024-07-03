b1412097db5a3a653b70e1595f3755d593e1b7a2
Kim Daehyeon <daehyeon3351@gmail.com>
2024-07-04 05:51:31 +0900

    feat: Controller 와 View 를 연결해주는 Application 작성 - #1
    - 중간 다리 역할
    - 예외 발생 시 재시도 구현
    - ApplicationConfig 를 통해 객체들을 얻어 옴

--- java-lotto-6/src/main/java/lotto/Application.java
+++ java-lotto-6/src/main/java/lotto/Application.java
@@ -1,7 +1,152 @@
 package lotto;
 
+import java.util.List;
+import lotto.common.config.ApplicationConfig;
+import lotto.controller.LottoController;
+import lotto.controller.WinningLottoController;
+import lotto.dto.request.CalculateLottoRequest;
+import lotto.dto.request.LottoRequest;
+import lotto.dto.request.PurchaseLottoRequest;
+import lotto.dto.request.WinningLottoRequest;
+import lotto.dto.response.CalculateLottoResponse;
+import lotto.dto.response.PurchaseLottoResponse;
+import lotto.dto.response.WinningLottoResponse;
+import lotto.view.LottoInputView;
+import lotto.view.LottoOutputView;
+import lotto.view.WinningLottoInputView;
+import lotto.view.WinningLottoOutputView;
+
 public class Application {
+    // ApplicationConfig 를 통해 필요한 객체들을 얻는다.
+    private static final ApplicationConfig applicationConfig = new ApplicationConfig();
+
+    // Controllers
+    private static final LottoController lottoController = applicationConfig.lottoController();
+    private static final WinningLottoController winningLottoController = applicationConfig.winningLottoController();
+
+    // Input Views
+    private static final LottoInputView lottoInputView = applicationConfig.lottoInputView();
+    private static final LottoOutputView lottoOutputView = applicationConfig.lottoOutputView();
+
+    // Output Views
+    private static final WinningLottoInputView winningLottoInputView = applicationConfig.winningLottoInputView();
+    private static final WinningLottoOutputView winningLottoOutputView = applicationConfig.winningLottoOutputView();
+
+    /**
+     * 애플리케이션 실행
+     */
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            // 로또 구매
+            PurchaseLottoResponse purchaseLottoResponse = purchaseLotto();
+
+            // 당첨 로또 생성
+            WinningLottoResponse winningLottoResponse = createWinningLotto();
+
+            // 로또 결과 계산
+            CalculateLottoRequest calculateLottoRequest = prepareCalculateLottoRequest(purchaseLottoResponse,
+                    winningLottoResponse);
+
+            // 결과 출력
+            printResult(calculateLottoRequest);
+        } catch (Exception e) {
+            System.out.println("[ERROR] 알 수 없는 오류가 발생했습니다.");
+        }
+    }
+
+    /**
+     * 로또 구매
+     *
+     * @return 로또 구매 응답
+     */
+    private static PurchaseLottoResponse purchaseLotto() {
+        while (true) {
+            try {
+                // 로또 구매 요청을 입력 받는다.
+                PurchaseLottoRequest purchaseLottoRequest = lottoInputView.inputPurchaseMoney();
+
+                // 로또 구매 요청을 통해 로또 구매 응답을 얻는다.
+                PurchaseLottoResponse purchaseLottoResponse = lottoController.purchaseLotto(purchaseLottoRequest);
+
+                // 로또 구매 응답을 출력한다.
+                lottoOutputView.printLottoPurchaseResult(purchaseLottoResponse);
+
+                // 로또 구매 응답을 반환한다.
+                return purchaseLottoResponse;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    /**
+     * 당첨 로또 생성
+     *
+     * @return 당첨 로또 응답
+     */
+    private static WinningLottoResponse createWinningLotto() {
+        while (true) {
+            try {
+                // 당첨 로또를 입력 받는다.
+                WinningLottoRequest winningLottoRequest = winningLottoInputView.inputWinningLotto();
+
+                // 당첨 로또를 생성한다.
+                WinningLottoResponse winningLottoResponse = winningLottoController.createWinningLotto(
+                        winningLottoRequest);
+
+                // 당첨 로또 응답을 반환한다.
+                return winningLottoResponse;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    /**
+     * 로또 결과 계산을 위한 요청을 준비한다.
+     *
+     * @param purchaseLottoResponse 로또 구매 응답
+     * @param winningLottoResponse  당첨 로또 응답
+     * @return 로또 계산 요청
+     */
+    private static CalculateLottoRequest prepareCalculateLottoRequest(PurchaseLottoResponse purchaseLottoResponse,
+                                                                      WinningLottoResponse winningLottoResponse) {
+        while (true) {
+            try {
+                // 로또 구매 응답에서 사용자 로또 요청 List 를 추출한다.
+                List<LottoRequest> lottoRequests = purchaseLottoResponse.getLottoResponses().stream()
+                        .map(lottoResponse -> new LottoRequest(lottoResponse.getNumbers()))
+                        .toList();
+
+                // 당첨 로또 응답을 통해 당첨 로또 요청 객체를 생성한다.
+                WinningLottoRequest winningLottoRequest = new WinningLottoRequest(winningLottoResponse.getNumbers(),
+                        winningLottoResponse.getBonusNumber());
+
+                // 사용자 로또 요청 List 와 당첨 로또 요청 객체를 통해 로또 계산 요청 객체를 생성한다.
+                return new CalculateLottoRequest(winningLottoRequest, lottoRequests);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    /**
+     * 로또 결과 출력
+     *
+     * @param calculateLottoRequest 로또 계산 요청
+     */
+    private static void printResult(CalculateLottoRequest calculateLottoRequest) {
+        while (true) {
+            try {
+                CalculateLottoResponse calculateLottoResponse = winningLottoController.CalculateLotto(
+                        calculateLottoRequest);
+                winningLottoOutputView.printWinningResult(calculateLottoResponse);
+
+                return;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
+
