dfcf42a2df654fc0f8fc829e81674b83df084e56
Kim Daehyeon <daehyeon3351@gmail.com>
2024-07-10 23:05:45 +0900

    refactor: 커스텀 예외를 던지도록 수정, 로직 분리 - #1

--- java-lotto-6/src/main/java/lotto/view/LottoInputView.java
+++ java-lotto-6/src/main/java/lotto/view/LottoInputView.java
@@ -1,8 +1,9 @@
 package lotto.view;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
-import static lotto.common.constant.ErrorMessageConstants.INVALID_INPUT_NUMBER;
+import static lotto.common.exception.ExceptionFactory.getCustomException;
 
+import lotto.common.exception.CustomExceptionType;
 import lotto.dto.request.PurchaseLottoRequest;
 
 /**
@@ -17,10 +18,11 @@ public class LottoInputView {
     public PurchaseLottoRequest inputPurchaseMoney() {
         try {
             System.out.println("구입금액을 입력해 주세요.");
-            int purchaseMoney = Integer.parseInt(readLine());
+            String input = readLine();
+            int purchaseMoney = Integer.parseInt(input);
             return new PurchaseLottoRequest(purchaseMoney);
         } catch (IllegalArgumentException e) {
-            throw new IllegalArgumentException(INVALID_INPUT_NUMBER);
+            throw getCustomException(CustomExceptionType.INVALID_NUMBER_INPUT);
         }
     }
 }
