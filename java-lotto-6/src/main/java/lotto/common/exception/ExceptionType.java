ccaf82a1599c8ec45013dfefad52dd128ce6d297
Kim Daehyeon <daehyeon3351@gmail.com>
2024-07-10 22:57:11 +0900

    feat: 커스텀 예외 별 Enum 추가 - #1
    - Factory 에서 사용될 예정
    - 예외 클래스를 가짐

---
+++ java-lotto-6/src/main/java/lotto/common/exception/ExceptionType.java
@@ -0,0 +1,32 @@
+package lotto.common.exception;
+
+import lotto.common.exception.classes.BonusNumberConflictException;
+import lotto.common.exception.classes.CustomException;
+import lotto.common.exception.classes.InvalidBonusNumberRangeException;
+import lotto.common.exception.classes.InvalidLottoRangeException;
+import lotto.common.exception.classes.InvalidLottoSizeException;
+import lotto.common.exception.classes.InvalidNumberInputException;
+import lotto.common.exception.classes.LottoMoneyTooSmallException;
+import lotto.common.exception.classes.LottoNumberDuplicateException;
+import lotto.common.exception.classes.LottoPurchaseAmountNotMultipleException;
+
+public enum ExceptionType {
+    INVALID_LOTTO_RANGE(InvalidLottoRangeException.class),// 로또 번호 범위가 유효하지 않은 경우
+    BONUS_NUMBER_CONFLICT(BonusNumberConflictException.class),// 보너스 번호가 당첨 번호와 중복되는 경우
+    INVALID_BONUS_NUMBER_RANGE(InvalidBonusNumberRangeException.class),// 보너스 번호 범위가 유효하지 않은 경우
+    INVALID_LOTTO_SIZE(InvalidLottoSizeException.class),// 로또 번호 개수가 유효하지 않은 경우
+    INVALID_NUMBER_INPUT(InvalidNumberInputException.class),// 입력한 숫자가 유효하지 않은 경우
+    LOTTO_MONEY_TOO_SMALL(LottoMoneyTooSmallException.class),// 로또 구매 금액이 유효하지 않은 경우
+    LOTTO_NUMBER_DUPLICATE(LottoNumberDuplicateException.class),// 로또 번호가 중복되는 경우
+    LOTTO_PURCHASE_AMOUNT_NOT_MULTIPLE(LottoPurchaseAmountNotMultipleException.class);// 로또 구매 금액이 유효하지 않은 경우
+
+    private final Class<? extends CustomException> exceptionClass;
+
+    ExceptionType(Class<? extends CustomException> exceptionClass) {
+        this.exceptionClass = exceptionClass;
+    }
+
+    public Class<? extends CustomException> getExceptionClass() {
+        return exceptionClass;
+    }
+}
