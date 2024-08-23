167015b6042f9038c2b2dc585c4c61d990ca156f
Kim Daehyeon <daehyeon3351@gmail.com>
2024-08-23 21:53:31 +0900

    refactor: 상수를 사용하도록 변경 - #5

--- java-baseball-6/src/main/java/baseball/baseball_game_client/common/message/ErrorMessage.java
+++ java-baseball-6/src/main/java/baseball/baseball_game_client/common/message/ErrorMessage.java
@@ -1,5 +1,8 @@
 package baseball.baseball_game_client.common.message;
 
+import static baseball.baseball_game_client.common.constant.Constants.BASEBALL_GAME_END;
+import static baseball.baseball_game_client.common.constant.Constants.BASEBALL_GAME_RESTART;
+
 /**
  * 에러 메시지를 관리하는 클래스
  */
@@ -7,5 +10,6 @@ public class ErrorMessage {
 
   public static final String INVALID_INPUT_ERROR = "잘못된 입력입니다.";
   public static final String INPUT_NOT_NUMBER_ERROR = "숫자가 아닌 값이 입력되었습니다.";
-  public static final String INVALID_RETRY_STATE_ERROR = "1 또는 2만 입력 가능합니다.";
+  public static final String INVALID_RETRY_STATE_ERROR =
+      BASEBALL_GAME_RESTART + " 또는 " + BASEBALL_GAME_END + "만 입력 가능합니다.";
 }
