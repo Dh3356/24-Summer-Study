c2b590fcefd77ff471ae3cfc5772995c4c6a27ce
Kim Daehyeon <daehyeon3351@gmail.com>
2024-08-23 22:45:08 +0900

    feat: 게임의 숫자 크기 정의 - #5

--- java-baseball-6/src/main/java/baseball/baseball_game_client/common/constant/Constants.java
+++ java-baseball-6/src/main/java/baseball/baseball_game_client/common/constant/Constants.java
@@ -2,6 +2,7 @@ package baseball.baseball_game_client.common.constant;
 
 public class Constants {
 
+  public static final int BASEBALL_GAME_NUMBER_SIZE = 3;
   public static final int BASEBALL_GAME_RESTART = 1;
   public static final int BASEBALL_GAME_END = 2;
 }
