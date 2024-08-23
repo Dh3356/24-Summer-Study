be23c0fb1ee658dea714817e6761c195f18cb10a
Kim Daehyeon <daehyeon3351@gmail.com>
2024-08-23 21:55:38 +0900

    feat: 출력에 필요한 매개변수가 있는 출력 뷰 인터페이스 정의 - #5

---
+++ java-baseball-6/src/main/java/baseball/baseball_game_client/view/output/OutputResultView.java
@@ -0,0 +1,16 @@
+package baseball.baseball_game_client.view.output;
+
+/**
+ * 출력에 필요한 매개변수가 있는 출력 뷰
+ *
+ * @param <T> 출력 타입
+ */
+public interface OutputResultView<T> {
+
+  /**
+   * 결과를 출력.
+   *
+   * @param t 출력
+   */
+  void print(T t);
+}
