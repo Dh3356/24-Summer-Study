4aaa382c58fe689f222c330ab5221139d9360829
Kim Daehyeon <daehyeon3351@gmail.com>
2024-08-23 21:50:31 +0900

    feat: Client 에서 사용할 에러 메시지 정의 - #5

---
+++ java-baseball-6/src/main/java/baseball/baseball_game_client/common/message/ErrorMessage.java
@@ -0,0 +1,11 @@
+package baseball.baseball_game_client.common.message;
+
+/**
+ * 에러 메시지를 관리하는 클래스
+ */
+public class ErrorMessage {
+
+  public static final String INVALID_INPUT_ERROR = "잘못된 입력입니다.";
+  public static final String INPUT_NOT_NUMBER_ERROR = "숫자가 아닌 값이 입력되었습니다.";
+  public static final String INVALID_RETRY_STATE_ERROR = "1 또는 2만 입력 가능합니다.";
+}
