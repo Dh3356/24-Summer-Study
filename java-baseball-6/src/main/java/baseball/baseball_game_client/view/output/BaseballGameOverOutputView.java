a9891a61b4ea27cfbaf9d3297975646de1a7b5cb
Kim Daehyeon <daehyeon3351@gmail.com>
2024-08-23 21:59:16 +0900

    feat: 게임 종료 출력 뷰 구현 - #5

---
+++ java-baseball-6/src/main/java/baseball/baseball_game_client/view/output/BaseballGameOverOutputView.java
@@ -0,0 +1,17 @@
+package baseball.baseball_game_client.view.output;
+
+import static baseball.baseball_game_client.view.output.message.OutputMessage.BASEBALL_GAME_OVER;
+
+/**
+ * 숫자 야구 게임 종료 출력 뷰
+ */
+public class BaseballGameOverOutputView implements OutputView {
+
+  /**
+   * 숫자 야구 게임 종료 메시지를 출력한다.
+   */
+  @Override
+  public void print() {
+    System.out.println(BASEBALL_GAME_OVER);
+  }
+}
