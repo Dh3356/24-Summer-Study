cc11fd375be83d1afe3f3203371340046e3cef0a
Kim Daehyeon <daehyeon3351@gmail.com>
2024-08-23 21:54:38 +0900

    feat: 입력 뷰 인터페이스 정의 - #5

---
+++ java-baseball-6/src/main/java/baseball/baseball_game_client/view/input/InputView.java
@@ -0,0 +1,21 @@
+package baseball.baseball_game_client.view.input;
+
+/**
+ * 입력 뷰
+ *
+ * @param <T> 입력 타입
+ */
+public interface InputView<T> {
+
+  /**
+   * 입력.
+   *
+   * @return 입력
+   */
+  T input();
+
+  /**
+   * 입력 전 출력.
+   */
+  void printBeforeInput();
+}
