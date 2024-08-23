689fd29429aadbeecbb3ec8354bb32fcc9950353
Kim Daehyeon <daehyeon3351@gmail.com>
2024-08-23 22:08:57 +0900

    feat: Protocol 에서 사용할 에러 메시지 정의 - #5

---
+++ java-baseball-6/src/main/java/baseball/protocol/common/message/ErrorMessage.java
@@ -0,0 +1,9 @@
+package baseball.protocol.common.message;
+
+/**
+ * Protocol 에러 메시지를 관리하는 클래스
+ */
+public class ErrorMessage {
+
+  public static final String SERVER_NOT_EXIST_ERROR = "해당 서버가 존재하지 않습니다.";
+}
