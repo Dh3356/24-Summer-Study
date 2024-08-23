0494bff013bf44eb450fb6bfe1375f3702eda10d
Kim Daehyeon <daehyeon3351@gmail.com>
2024-08-23 21:53:13 +0900

    feat: Client 에서 사용할 상수 정의 - #5

---
+++ java-baseball-6/src/main/java/baseball/baseball_game_client/common/constant/Constants.java
@@ -0,0 +1,7 @@
+package baseball.baseball_game_client.common.constant;
+
+public class Constants {
+
+  public static final int BASEBALL_GAME_RESTART = 1;
+  public static final int BASEBALL_GAME_END = 2;
+}
